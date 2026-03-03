e6a57d37214766e02bfb842cb329dbb2007a2102
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-02 23:57:01 -0500

    Initalize backend with node and express, including cors, nodemon, dotenv, pg dependancies and dbHealth middleware

---
+++ backend/src/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import cors from 'cors'
+import pool from './db/pools.js'
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+// Middleware for checking database health
+const dbHealth = async (req, res, next) => {
+    try {
+        await pool.query('SELECT 1');
+        next();
+    } catch (error) {
+        res.status(503).json({ 
+            message: 'Express server is healthy. Postgres database connection is down.',
+            server: 'up', 
+            database: 'down',
+            error: error.message 
+        });
+    }
+}
+
+app.use(express.json());
+app.use(cors());
+app.use(dbHealth)
+
+app.get('/', async (req, res) => {
+    res.status(200).json({ 
+        message: 'Express server is healthy. Postgres database connection is healthy.',
+        server: 'up',
+        database: 'up'
+    });
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Express server is running on port ${PORT}`);
+});
