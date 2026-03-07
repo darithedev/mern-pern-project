e2b290c7926f87c16f8d8950e059bde654b4f6c1
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-07 11:20:01 -0500

    Refactor individual form test file extension to jsx, import vitest test methods

--- frontend/src/components/IndividualForm.test.jsx
+++ frontend/src/components/IndividualForm.test.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import '@testing-library/jest-dom/vitest'
+import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
+import IndividualForm from './IndividualForm.jsx'
 
 describe('Individual Form Component', () => {
     test('individual form renders', () => {
