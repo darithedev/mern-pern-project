80da24dd4d5cf29cb49730ab0d7c6013ca28040e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-06 22:49:59 -0500

    Add test for Individual Form testing if form renders

--- frontend/src/components/IndividualForm.test.js
+++ frontend/src/components/IndividualForm.test.js
@@ -2,5 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 
 describe('Individual Form Component', () => {
-    
+    test('individual form renders', () => {
+        render(<IndividualForm />)
+        expect(screen.getByText('Add Individual Animal Form')).toBeInTheDocument();
+    });
 });
