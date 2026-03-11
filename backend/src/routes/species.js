129f9a5d3f75ce1cf5736c3da6c26bcaf7b546ef
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-11 19:58:25 -0400

    Add GET route for species

--- backend/src/routes/species.js
+++ backend/src/routes/species.js
@@ -3,4 +3,14 @@ import pool from '../db/pools.js'
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const result = await pool.query(`SELECT * FROM species`);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error with getting all species', error);
+        res.status(500).json({ error: 'Error! Could not get species' });
+    }
+});
+
 export default router;
