0017c83ef325082fd3f74c9b4fd417e5286d58ae
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-12 00:49:10 -0400

    Add species post route with controller logitc

--- backend/src/routes/species.js
+++ backend/src/routes/species.js
@@ -13,4 +13,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const { common_name, scientific_name, estimated_in_the_wild, conservation_code } = req.body;
+        const result = await pool.query(
+            `INSERT INTO species (common_name, scientific_name, estimated_in_the_wild, conservation_code)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *`,
+            [common_name, scientific_name, estimated_in_the_wild, conservation_code]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error with adding this specie.', error);
+        res.status(500).json({ error: 'Error! Could not add this specie.' });
+    }
+});
+
 export default router;
