fce1eedd837cf410e7af9c802e381accb1fe8537
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-11 20:16:52 -0400

    Add get endpoint with controller logic

--- backend/src/routes/individuals.js
+++ backend/src/routes/individuals.js
@@ -3,4 +3,14 @@ import pool from '../db/pools.js'
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const result = await pool.query(`SELECT * FROM individuals`);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error with getting all individual endangered animals.', error);
+        res.status(500).json({ error: 'Error! Could not get individual endangered animals' });
+    }
+});
+
 export default router;
