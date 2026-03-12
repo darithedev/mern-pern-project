a3b85458cb91db692c9c52549a7539cc99629c27
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-11 20:30:39 -0400

    Add post route for individuals and controller logic

--- backend/src/routes/individuals.js
+++ backend/src/routes/individuals.js
@@ -13,4 +13,21 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const { nick_name, scientist_tracking, species_id } = req.body;
+
+        const result = await pool.query(
+            `INSERT INTO individuals (nick_name, scientist_tracking, species_id)
+            VALUES ($1, $2, $3)
+            RETURNING *`,
+            [nick_name, scientist_tracking, species_id]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error with adding individual endangered animal.', error.message);
+        res.status(500).json({ error: 'Error! Could not add individual endangered animal.' });
+    }
+});
+
 export default router;
