db083edd08fee1e9090748798d8b9a1a9558540a
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-12 16:48:23 -0400

    Refactor post species route to account for conservation codes less than 5 characters

--- backend/src/routes/species.js
+++ backend/src/routes/species.js
@@ -36,9 +36,9 @@ router.post('/', async (req, res) => {
             });
         };
 
-        if (!conservation_code || conservation_code.length !== 2) {
+        if (!conservation_code || conservation_code.length > 5) {
             return res.status(400).json({
-                error: "A 2 character conservation code for this endangered animal is required!"
+                error: "A conservation code with less than 5 characters is required for this endangered animal!"
             });
         }
 
@@ -54,5 +54,24 @@ router.post('/', async (req, res) => {
         res.status(500).json({ error: 'Error! Could not add this specie.' });
     }
 });
+
+// This should be admin route only - Only admin can update 
+/*router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { common_name, scientific_name, estimated_in_the_wild, conservation_code } = req.body;
+        
+        const result = await pool.query(
+            'UPDATE species SET common_name=$1, scientific_name=$2, estimated_in_the_wild=$3, conservation_code=$4 WHERE id=$5 RETURNING *',
+            [common_name, scientific_name, estimated_in_the_wild, conservation_code, id]
+        );
+
+        if (result.rows.length === 0) return res.status(404).json({ error: 'This event does not exist!'});
+
+        res.status(201).json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});*/
 
 export default router;
