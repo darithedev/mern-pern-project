8c4f0fb7357a6ff76270e32192347395c3d32211
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-11 23:45:48 -0400

    Add get route for sightings with controller logic

--- backend/src/routes/sightings.js
+++ backend/src/routes/sightings.js
@@ -3,4 +3,14 @@ import pool from '../db/pools.js'
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const result = await pool.query(`SELECT * FROM sightings`);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error with getting all sightings.', error);
+        res.status(500).json({ error: 'Error! Could not get sightings.' });
+    }
+});
+
 export default router;
