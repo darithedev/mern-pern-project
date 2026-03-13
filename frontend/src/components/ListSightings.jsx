973919bfb416beeefbec230ab8302a42c8dfa0fd
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 18:33:19 -0400

    Add h2 header, ul and map, and button to go back to species screen

--- frontend/src/components/ListSightings.jsx
+++ frontend/src/components/ListSightings.jsx
@@ -18,7 +18,21 @@ const ListSightings = ({ updateScreen }) => {
 
     return (
         <div className="list-sightings-container">
-            
+            <h2>Animals Tracked Sightings</h2>
+            <ul className="sighting-card">
+                {sightings.map((sighting) => {
+                    return (
+                        <li className="sightings-card" key={sighting.id}>
+                            <p>Animal: {sighting.nick_name}</p>
+                            <p>Date Sighted: {sighting.sighting ? new Date(sighting.sighting).toLocaleDateString() : "No Sightings"}</p>
+                            <p>Location: {sighting.location}</p>
+                            <p>Health: {sighting.healthy ? "Healthy" : "Not Healthy"}</p>
+                            <p>Contact of Person Tracking: {sighting.sighted_by_email}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+            <button onClick={() => updateScreen("species")}>Go Back</button>
         </div>
     )
 }
