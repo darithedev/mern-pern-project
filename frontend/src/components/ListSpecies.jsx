6be7ecee5498d13e8e7157cd1dae7a81c84de3cb
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 12:21:42 -0400

    Add fetch for species to get all species in db from backend species api

--- frontend/src/components/ListSpecies.jsx
+++ frontend/src/components/ListSpecies.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 
 const ListSpecies = () => {
     const [species, setSpecies] = useState([]);
+
+    const fetchSpecies = () => {
+        fetch("http://localhost:8080/api/species")
+        .then((response) => response.json())
+        .then((endangeredSpecies) => {
+          setIndividuals(endangeredSpecies);
+        });
+    };
+
     return (
         <div className="species-container">
             <p>Hello Species List </p>
