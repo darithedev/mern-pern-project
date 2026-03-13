51d621ba14234cf902bca2f7da91c0731e9f11c4
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 18:26:40 -0400

    Add fetchSightings to get all listed sighting from backend sightings api, add useEffect for fetchSighting, and refactor listSightings component prop to updateScreen

--- frontend/src/components/ListSightings.jsx
+++ frontend/src/components/ListSightings.jsx
@@ -1,8 +1,20 @@
 import { useState, useEffect } from 'react'
 
-const ListSightings = ({ setScreen }) => {
+const ListSightings = ({ updateScreen }) => {
     const [sightings, setSightings] = useState([]);
     const [editingSighting, setEditingStghting] = useState(null);
+
+    const fetchSightings = () => {
+      fetch("http://localhost:8080/api/sightings")
+        .then((response) => response.json())
+        .then((individualSightings) => {
+          setSightings(individualSightings);
+        });
+    };
+
+    useEffect(() => {
+        fetchSightings();
+    }, []);
 
     return (
         <div className="list-sightings-container">
