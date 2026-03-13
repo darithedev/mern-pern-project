dadba94f58f1596293320157be850a8f03fd4a36
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 12:19:31 -0400

    Import useState and add species with setter

--- frontend/src/components/ListSpecies.jsx
+++ frontend/src/components/ListSpecies.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 const ListSpecies = () => {
+    const [species, setSpecies] = useState([]);
     return (
         <div className="species-container">
             <p>Hello Species List </p>
