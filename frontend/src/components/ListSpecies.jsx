05517e38f3b86e15927af947851498d0ad4553ee
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 12:15:27 -0400

    Add ListSpecies component skeleton

---
+++ frontend/src/components/ListSpecies.jsx
@@ -0,0 +1,9 @@
+const ListSpecies = () => {
+    return (
+        <div className="species-container">
+            <p>Hello Species List </p>
+        </div>
+    )
+}
+
+export default ListSpecies;
