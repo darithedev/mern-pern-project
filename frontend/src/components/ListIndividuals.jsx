26e483d793c9c294bd0887704349608d8a8ba185
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 09:41:12 -0400

    Import useState, add individual and sightings useState and setters

--- frontend/src/components/ListIndividuals.jsx
+++ frontend/src/components/ListIndividuals.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+
 const ListIndividuals = () => {
+    const [individuals, setIndividuals] = useState([]);
+    const [sightings, setSightings] = useState([]);
+    
     return (
         <div className="individual-container">
             <p>Hello List Individuals</p>
