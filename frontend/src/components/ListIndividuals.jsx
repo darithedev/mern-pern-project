1d42cd2569ad72eb675a5e01c136fd6ae228f343
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 09:36:15 -0400

    Add skeleton of ListIndividuals component

---
+++ frontend/src/components/ListIndividuals.jsx
@@ -0,0 +1,9 @@
+const ListIndividuals = () => {
+    return (
+        <div className="individual-container">
+            <p>Hello List Individuals</p>
+        </div>
+    )
+}
+
+export default ListIndividuals
