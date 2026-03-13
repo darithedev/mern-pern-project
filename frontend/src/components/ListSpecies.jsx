b196ecdb2e8ec71e87052e5a0b75f2e0964113bb
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 13:06:29 -0400

    Add conservation codes object in codes array

--- frontend/src/components/ListSpecies.jsx
+++ frontend/src/components/ListSpecies.jsx
@@ -2,6 +2,17 @@ import { useState, useEffect } from 'react'
 
 const ListSpecies = () => {
     const [species, setSpecies] = useState([]);
+    const codes = [
+        { code: "EX", label: "Extinct" },
+        { code: "EW", label: "Extinct in the Wild" },
+        { code: "CR", label: "Critically Endangered" },
+        { code: "EN", label: "Endangered" },
+        { code: "VU", label: "Vulnerable" },
+        { code: "NT", label: "Near Threatened" },
+        { code: "LC", label: "Least Concern" },
+        { code: "DD", label: "Data Deficient" },
+        { code: "NE", label: "Not Evaluated" }
+    ];
 
     const fetchSpecies = () => {
         fetch("http://localhost:8080/api/species")
