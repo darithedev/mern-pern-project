b0aa9707a93465a77eddfbaf9ccfcf2083e5e769
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 18:22:13 -0400

    Add ListSightings component skeleton with sightings and editSightings useState and setters, setScreen prop, import useState and useEffect

---
+++ frontend/src/components/ListSightings.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+
+const ListSightings = ({ setScreen }) => {
+    const [sightings, setSightings] = useState([]);
+    const [editingSighting, setEditingStghting] = useState(null);
+
+    return (
+        <div className="list-sightings-container">
+            
+        </div>
+    )
+}
+
+export default ListSightings;
