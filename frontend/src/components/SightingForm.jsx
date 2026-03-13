5f922d0295061331af03507dc6b3e2d48b1ee068
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 18:48:04 -0400

    Add SightingForm component skeleton

---
+++ frontend/src/components/SightingForm.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const SightingForm = ({ setScreen, speciesId, filteredIndivs }) => {
+
+    return (
+        <Form 
+            className="form-sightings"
+            onSubmit={handleSubmit}
+        >
+            
+        </Form>
+    )
+
+}
+
+export default SightingForm; 
+
