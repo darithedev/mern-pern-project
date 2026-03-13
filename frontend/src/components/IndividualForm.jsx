2cd7a5a571c1dc61f8f7f908427d47982ed1df22
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 15:55:34 -0400

    Add IndividualForm component with setScreenm speciesId, and speciesName, props, handleSubmit, and Form

---
+++ frontend/src/components/IndividualForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const IndividualForm = ({ setScreen, speciesId, speciesName }) => {
+
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <Form 
+            className="form-individual"
+            onSubmit={handleSubmit}
+        >
+            
+        </Form>
+    )
+
+}
+
+export default IndividualForm; 
+
