8f664b26c5c6b1f24367ee673f10432dde43b620
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 12:16:48 -0400

    Refactor App.jsx to render imported ListSpecies component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ListSpecies from './components/Listspecies.jsx'
 import ListIndividuals from './components/ListIndividuals.jsx'
 
 function App() {
 
   return (
     <>
-      <ListIndividuals />
+      <ListSpecies />
     </>
   )
 }
