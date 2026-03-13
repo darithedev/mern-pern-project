f86d99a13019f0810edf2ba8db948bfeaad9ab48
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 09:49:39 -0400

    Add fetch for individuals and set individuals

--- frontend/src/components/ListIndividuals.jsx
+++ frontend/src/components/ListIndividuals.jsx
@@ -3,6 +3,14 @@ import { useState } from 'react'
 const ListIndividuals = () => {
     const [individuals, setIndividuals] = useState([]);
     const [sightings, setSightings] = useState([]);
+
+    const fetchIndividuals = () => {
+        fetch("http://localhost:8080/api/individuals")
+        .then((response) => response.json())
+        .then((individualAnimals) => {
+          setIndividuals(individualAnimals);
+        });
+    };
     
     return (
         <div className="individual-container">
