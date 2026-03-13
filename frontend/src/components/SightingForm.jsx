874271caebdd36d017cb256abf395b5bfe08814f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 19:46:15 -0400

    Add dateFormat helper, and imprt dateFormat to SightingForm component

--- frontend/src/components/SightingForm.jsx
+++ frontend/src/components/SightingForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import dateFormat from '../helpers/dateFormat';
 
 const SightingForm = ({ setScreen, speciesId, filteredIndivs }) => {
     
