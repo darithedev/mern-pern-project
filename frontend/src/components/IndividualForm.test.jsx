ec90ab76f413ebafb8be12a87319d6818c364fa1
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-13 21:10:18 -0400

    Add vitest for IndividualForm component and install testing-library user-event

--- frontend/src/components/IndividualForm.test.jsx
+++ frontend/src/components/IndividualForm.test.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import '@testing-library/jest-dom/vitest'
-import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, test, expect, afterEach } from 'vitest'
+import { render, screen, cleanup } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import IndividualForm from './IndividualForm.jsx'
+
+afterEach(() => {
+    cleanup()
+})
 
 describe('Individual Form Component', () => {
     test('individual form renders', () => {
         render(<IndividualForm />)
         expect(screen.getByText('Add Individual Animal Form')).toBeInTheDocument();
+    });
+
+    test('user should be able to input individual (animal) nick name', async () => {
+        render(<IndividualForm />)
+        const nickNameInput = screen.getByPlaceholderText('Cutie pie')
+        await userEvent.type(nickNameInput, 'Simba')
+        expect(nickNameInput).toHaveValue('Simba')
+    });
+
+    test('user should be able to provide their name', async () => {
+        render(<IndividualForm />)
+        const scientistInput = screen.getByPlaceholderText('Dari Cares')
+        await userEvent.type(scientistInput, 'Jane Goodall')
+        expect(scientistInput).toHaveValue('Jane Goodall')
     });
 });
